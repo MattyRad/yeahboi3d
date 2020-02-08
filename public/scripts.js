58c2fb4ec16761eb4efc8438f5136ab1eb2d8a01
Matt Radford <mradford@protonmail.com>
2020-02-07 17:10:45 -0800

    Prevent the cuboid from spinning, dizzy

--- public/scripts.js
+++ public/scripts.js
@@ -15,9 +15,6 @@ camera.position.z = 5;
 var animate = function () {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
-
 	renderer.render(scene, camera);
 };
 
