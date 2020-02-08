3053199bf1b375a241812083f15577772bf22d1c
Matt Radford <mradford@protonmail.com>
2020-02-07 17:09:53 -0800

    Change the original cube into a cuboid
    (Yes, I had to look up the word for "3d rectangle")

--- public/scripts.js
+++ public/scripts.js
@@ -5,7 +5,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var geometry = new THREE.BoxGeometry( 0.2, 1, 0.2 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
