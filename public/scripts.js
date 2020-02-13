32eba59db438fe4d92e160a43a82be24122fe64d
Matt Radford <mradford@protonmail.com>
2020-02-13 07:34:42 -0800

    Change the obstacle material to Standard and add some lights

--- public/scripts.js
+++ public/scripts.js
@@ -20,12 +20,25 @@ var obstacles = [];
 
 for (var i = 0; i < 100; i++) {
     var geometry = new THREE.BoxGeometry( 0.2, 1, 0.2 );
-    var material = new THREE.MeshNormalMaterial( { color: 0xff0000 } );
+    var material = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
     var obstacle = new THREE.Mesh( geometry, material );
     obstacle.position.x = 10 + Math.floor(Math.random() * 300);
     obstacles.push(obstacle);
     scene.add( obstacle );
 }
+
+var lights = [];
+lights[0] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[1] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[2] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+lights[0].position.set(0, 200, 0);
+lights[1].position.set(100, 200, 100);
+lights[2].position.set(-100, -200, -100);
+
+scene.add(lights[0]);
+scene.add(lights[1]);
+scene.add(lights[2]);
 
 var jumping = false;
 var jump_distance = 0.1;
